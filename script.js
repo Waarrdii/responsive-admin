ebaab84026901ef9aaa9ea8ea5ceaff098f6454f
waarrdii <waarrdii@gmail.com>
2023-11-30 19:52:35 +0700

    forEach

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const product = document.getElementById('product');
-const data = Array.from(product.querySelectorAll('tr'));
+const dataProduk = document.querySelectorAll('tr');
 
-console.log(data);
+const listProduk = [...dataProduk]
 
-if (data.length === 0) {
-    console.error('No rows found in the table.');
-  } else {
-    data.map((row) => {
-      const name = row.querySelector('td:first-child').textContent;
-      const age = row.querySelector('td:nth-child(2)').textContent;
-      const address = row.querySelector('td:nth-child(3)').textContent;
-  
-      return {
-        name,
-        age,
-        address,
-      };
-    });
-    console.log(data);
-}
+console.log(listProduk);
