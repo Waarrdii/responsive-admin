12c759aff788764c7546b0febdd97cda2f9e261c
waarrdii <waarrdii@gmail.com>
2023-12-01 18:06:22 +0700

    membuat table dinamis

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const dataProduk = document.querySelectorAll('tr');
+function generateData(){
+    let product = [
+        ["FUR-BO-10001798","Furniture","Bookcases","Bush Somerset Collection Bookcase",],
+        ["FUR-CH-10000454","Furniture","Chairs","Hon Deluxe Fabric Upholstered Stacking Chairs Rounded Back",],
+        ["OFF-LA-10000240","Office Supplies","Labels","Self-Adhesive Address Labels for Typewriters by Universal",],
+        ["FUR-TA-10000577","Furniture","Tables","Bretford CR4500 Series Slim Rectangular Table",],
+        ["OFF-ST-10000760","Office Supplies","Storage","Eldon Fold 'N Roll Cart System",],
+        ["FUR-FU-10001487","Furniture","Furnishings","Eldon Expressions Wood and Plastic Desk Accessories Cherry Wood",],
+        ["OFF-AR-10002833","Office Supplies","Art","Newell 322",],
+        ["TEC-PH-10002275","Technology","Phones","Mitel 5320 IP Phone VoIP phone",],
+        ["OFF-BI-10003910","Office Supplies","Binders","DXL Angle-View Binders with Locking Rings by Samsill",],
+    ];
 
-const listProduk = [...dataProduk]
-
-console.log(listProduk);
+    for (let i = 0; i<4; i++){
+        const row = document.createElement('tr');
+        for (let j = 0; j<9; j++){
+            const column = document.createElement('td');
+        }
+    }
+};
