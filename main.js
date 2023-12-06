48795dfb40248dc9378e709eae7437b2c5576fcd
waarrdii <waarrdii@gmail.com>
2023-12-07 00:00:49 +0700

    insert logo gagal src

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
+import {addLogo} from "./modules/Logo.js";
+window.addEventListener("load", addLogo);
+
 import {createTable} from "./modules/script.js";
 window.addEventListener("load", createTable);
--- modules/Logo.js
+++ modules/Logo.js
@@ -1,13 +1,8 @@
-function logo(){
-    const imgLogo = 
+function addLogo (){
+    const logo = document.querySelector('.logo');
+    const imgLogo = document.createElement('img');
+    imgLogo.textContent = 'src="./asset/Risoles.png" alt="wulan kitchen"';
+    logo.appendChild(imgLogo);
 }
 
-{/* <div class="logo">
-                    <img src="./asset/Risoles.png" alt="wulan kitchen">
-                    <div class="company-title">
-                        <h2>Wulan's</h2>
-                        <div class="line-title"></div>
-                        <span>kitchen</span>
-                    </div>
-                </div>
-                <div class="close" id="closeBtn"></div> */}
+export {addLogo} ;
