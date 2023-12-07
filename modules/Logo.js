871dbf5eb252896d942e33e6d296ee41eb550f12
waarrdii <waarrdii@gmail.com>
2023-12-07 23:09:06 +0700

    pemisahan logo done

--- modules/Logo.js
+++ modules/Logo.js
@@ -1,8 +1,17 @@
 function addLogo (){
-    const logo = document.querySelector('.logo');
-    const imgLogo = document.createElement('img');
-    imgLogo.textContent = 'src="./asset/Risoles.png" alt="wulan kitchen"';
-    logo.appendChild(imgLogo);
+    const logo = document.querySelector('.top');
+    const imgLogo = document.createElement('div');
+    imgLogo.classList.add('logo');
+    imgLogo.innerHTML = 
+    '<img src="./asset/Risoles.png" alt="wulan kitchen">'
+    + 
+    '<div class="company-title">' 
+    +   "<h2>Wulan's</h2>"
+    +   '<div class="line-title"></div>' 
+    +   '<span>kitchen</span>'
+    +'</div>'
+    +'<div class="close" id="closeBtn"></div>';
+   logo.append(imgLogo,);
 }
 
 export {addLogo} ;
