282c2a45dd7197282bb967e6f76d4ccae5819541
waarrdii <waarrdii@gmail.com>
2023-12-11 23:24:57 +0700

    loop card

--- modules/card.js
+++ modules/card.js
@@ -1,4 +1,12 @@
 function card (){
 const main = document.querySelector('main');
+const h1 = document.createElement('h2');
+h1.textContent = 'dasboard';
 
+const card = document.createElement('div');
+card.className = 'card';
+
+for (let i = 0; i<3; i++ ){
+
+}
 };
