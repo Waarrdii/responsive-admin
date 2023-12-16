b1ea81ce3efdc9860724ecfa2cc13356f6e92423
waarrdii <waarrdii@gmail.com>
2023-12-16 13:11:27 +0700

    styling sorting table and read readme.txt

--- modules/sort.js
+++ modules/sort.js
@@ -1,11 +1,23 @@
 function sort(){
     const table = document.querySelector('table');
-    const ths = table.querySelector("th:nth-of-type(2)");
-    console.log(ths.textContent);
-    ths.addEventListener("click", function(){
-        ths.classList.add("sorted");
-    });
-    
+    const ths = table.querySelectorAll("th");
+    for (const th of ths){
+        th.addEventListener("click",function(){
+            for (const other of ths){
+                if(other !== this){
+                    other.classList.remove('asc');
+                    other.classList.remove('desc');
+                }
+            }
+        if(th.classList.contains('asc')){
+            th.classList.remove('asc');
+            th.classList.add('desc');
+        }else{
+            th.classList.remove('desc');
+            th.classList.add('asc');
+        }
+        });
+    };
 };
 
 export {sort};
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
+16 Des 23, 13:10 => style sorting table done, ketika di klick 2 kali text pada header otomatis di block. cara agar tidak di block
+
 12 des 23, 00:53 => 1. pemisahan css dan mempelajari SCSS atau SASS;
                     2. Membuat nav page pada data table, tampilkan maksimal 5 data per halaman.
                     3. Membuat search, filter dan sort di tabel; 
